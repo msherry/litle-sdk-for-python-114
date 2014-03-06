dfbd9003f0aa1bd7cef02cdf88ed0c3848ade8cb
Marc Sherry <msherry@gmail.com>
2014-03-05 19:38:21 -0800

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 #from distutils.core import setup
 from setuptools import setup, find_packages
 setup(name='LitleSdkPython-114',
-      version='8.13.1',
+      version='8.13.1-1',
       description='Litle & Co. SDK for Python',
       author='Litle & Co',
       author_email='SDKSupport@litle.com',
